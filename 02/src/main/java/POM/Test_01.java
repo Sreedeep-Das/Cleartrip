a568e46d7a7ee1060540bc04643b22d78db2164d
TYSS <TYSS@TYSS-4F-045>
2017-05-17 17:38:54 +0530

    Hi! changes done ....by Santadeep

--- 02/src/main/java/POM/Test_01.java
+++ 02/src/main/java/POM/Test_01.java
@@ -1,6 +1,9 @@
 package POM;
 
-public class Test_01 extends BaseClass {
-	
-
+public class Test_01 extends BaseClass 
+{	
+	public static void main(String[] args) 
+	{
+		System.out.println("hI sANTADEEP .... TUM BOHOT BARAAAA C...");
+	}
 }
